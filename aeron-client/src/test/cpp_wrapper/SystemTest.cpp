1b936a644170a26f0f81d9526ae86a3797ef1c41
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2026-02-26 12:31:38 +0100

    Revert "[C++ Wrapper] Clean pending resource maps when `Aeron` instance is closed."
    This reverts commit d839e088548058b9478b12ed36bc5fe2a4905f3e.

--- aeron-client/src/test/cpp_wrapper/SystemTest.cpp
+++ aeron-client/src/test/cpp_wrapper/SystemTest.cpp
@@ -52,7 +52,8 @@ protected:
     EmbeddedMediaDriver m_driver;
 };
 
-TEST_F(SystemTest, shouldReclaimSubscriptionWhenOutOfScopeAndNotFound)
+// TODO: We need a way to clean up unresolved aeron_client_registering_resource_t* commands
+TEST_F(SystemTest, DISABLED_shouldReclaimSubscriptionWhenOutOfScopeAndNotFound)
 {
     std::shared_ptr<Aeron> aeron = Aeron::connect();
 
@@ -236,46 +237,6 @@ TEST_F(SystemTest, shouldFreeSubscriptionDataCorrectlyWithInvoker)
         }
         while (nullptr == subscription);
     }
-}
-
-TEST_F(SystemTest, shouldFreeUnclaimedSubscriptions)
-{
-    Context ctx;
-    ctx.useConductorAgentInvoker(false);
-
-    std::shared_ptr<Aeron> aeron = Aeron::connect(ctx);
-    aeron->addSubscription("aeron:ipc", 1000);
-    aeron->addSubscription("aeron:ipc", 2000);
-}
-
-TEST_F(SystemTest, shouldFreeUnclaimedPublications)
-{
-    Context ctx;
-    ctx.useConductorAgentInvoker(false);
-
-    std::shared_ptr<Aeron> aeron = Aeron::connect(ctx);
-    aeron->addPublication("aeron:ipc", 1000);
-    aeron->addPublication("aeron:ipc", 2000);
-}
-
-TEST_F(SystemTest, shouldFreeUnclaimedExclusivePublications)
-{
-    Context ctx;
-    ctx.useConductorAgentInvoker(false);
-
-    std::shared_ptr<Aeron> aeron = Aeron::connect(ctx);
-    aeron->addExclusivePublication("aeron:udp?endpoint=localhost:5555", 1000);
-    aeron->addExclusivePublication("aeron:ipc", 2000);
-}
-
-TEST_F(SystemTest, shouldFreeUnclaimedCounters)
-{
-    Context ctx;
-    ctx.useConductorAgentInvoker(false);
-
-    std::shared_ptr<Aeron> aeron = Aeron::connect(ctx);
-    aeron->addCounter(1000, nullptr, 0, "test");
-    aeron->addCounter(2000, nullptr, 0, "another");
 }
 
 class SystemTestParameterized : public testing::TestWithParam<std::string>
