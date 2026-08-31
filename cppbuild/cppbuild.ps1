77d3bc1f44c2d12ad74174ff2261c87620d823fb
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2026-08-31 10:25:24 +0200

    [C] Upgrade to `CMake` 4.4.3.

--- cppbuild/cppbuild.ps1
+++ cppbuild/cppbuild.ps1
@@ -164,7 +164,7 @@ for ($i = 0; $i -lt $Args.count; $i++)
 
 $BuildDir = "$PSScriptRoot\$BuildConfig"
 $SourceDir = "$PSScriptRoot\.."
-$CMakeVersion = "4.3.4"
+$CMakeVersion = "4.4.3"
 $CMakeArch = "x86_64"
 if ( $env:PROCESSOR_ARCHITECTURE -eq "ARM64" )
 {
